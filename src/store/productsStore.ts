c554242757af7d98f52dd360615f04a183dcc4cf
FlavioMontoril <flavio.montorilll@gmail.com>
2025-08-26 20:00:25 -0300

    refactor: add functions necessary

--- src/store/productsStore.ts
+++ src/store/productsStore.ts
@@ -1,33 +1,17 @@
-// import { ProductProps } from "@/types/FilterStatus";
-// import { create } from "zustand";
-
-// interface ProductPropsStore {
-//     products: ProductProps[],
-//     addProduct: (data: ProductProps) => void,
-// }
-
-// export const useProductStore = create<ProductPropsStore>((set) => {
-
-//     function handleAddProduct(data: ProductProps) {
-//         set((prev) => {
-//             const exist = prev.products.some(item => item.id === data.id)
-//             return { products: exist ? prev.products : [...prev.products, data], }
-//         })
-//     }
-//     return {
-//         products: [],
-//         addProduct: handleAddProduct,
-//     }
-// })
-
-import { ProductProps } from "@/types/FilterStatus";
+import { FilterStatus, ProductProps } from "@/types/FilterStatus";
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import uuid from "react-native-uuid";
+
+type UpdatedProduct = Omit<ProductProps, "id" | "description">
 
 interface ProductPropsStore {
     products: ProductProps[];
-    addProduct: (data: ProductProps) => void;
+    addProduct: (description: string) => void;
+    removeProduct: (id: string) => void;
+    clearProducts: () => void;
+    updateStatusProduct: (id: string) => void;
 }
 
 export const useProductStore = create<ProductPropsStore>()(
@@ -35,13 +19,36 @@ export const useProductStore = create<ProductPropsStore>()(
         (set) => ({
             products: [],
 
-            addProduct: (data: ProductProps) =>
+            addProduct: (description: string) =>
                 set((prev) => {
-                    const exists = prev.products.some((item) => item.id === data.id);
+                    const newProduct = {
+                        id: uuid.v4() as string,
+                        description,
+                        status: FilterStatus.PENDING,
+                    }
+                    const exists = prev.products.some((item) => item.id === newProduct.id);
                     return {
-                        products: exists ? prev.products : [...prev.products, data],
+                        products: exists ? prev.products : [...prev.products, newProduct],
                     };
                 }),
+
+            removeProduct: (id: string) =>
+                set((prev) => ({ products: prev.products.filter(item => item.id !== id) })),
+
+            clearProducts: () =>
+                set(() => ({ products: [] })),
+
+            updateStatusProduct: (id: string) =>
+                set((prev) => ({
+                    products: prev.products.map((item) => item.id === id ?
+                        {
+                            ...item,
+                            status: item.status === FilterStatus.PENDING
+                                ? FilterStatus.DONE : FilterStatus.PENDING
+                        }
+                        : item)
+                }
+                ))
         }),
         {
             name: "product-storage", // chave no AsyncStorage
